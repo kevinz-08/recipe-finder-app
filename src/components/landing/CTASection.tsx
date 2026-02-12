724b18ea0b3de77a9a90233a4fefc48925d1b1c2
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2026-02-11 23:50:46 -0500

    feat: :art: create HeroSection with responsive layout and CTA

--- src/components/landing/CTASection.tsx
+++ src/components/landing/CTASection.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom"
 import { ROUTES } from "../../app";
-import { Button } from "../shared"
+// import { Button } from "../shared"
 
 export const CTAsection = () => {
     return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-slate-50">
 
-        <h1 className="text-3xl font-bold text-slate-800">Hola!</h1>
+        <h1 className="text-3xl font-bold text-slate-800">Zona del CallToAction</h1>
         <p className="text-slate-600">Bienvenido</p>
 
         <Link to={ROUTES.LOGIN} className="mt-4 text-blue-500 underline">
@@ -15,12 +15,6 @@ export const CTAsection = () => {
 
         <Link to={ROUTES.DASHBOARD} className="mt-4 text-blue-500 underline">
             Prueba Dasboard
-        </Link>
-
-        <Link to={ROUTES.LOGIN}>
-            <Button>
-                Comienza Ahora
-            </Button>
         </Link>
 
     </div>
