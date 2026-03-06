13ba86d7651997230865c866d1f2fae05499785e
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2026-03-06 17:40:23 -0500

    Refactor Sidebar and ContactSection components

--- src/components/layout/PortalLayout.tsx
+++ src/components/layout/PortalLayout.tsx
@@ -1,25 +1,15 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "./SideBar";
+import Sidebar from "./Sidebar";
 
 export default function PortalLayout() {
 
-    // }
     return (
       <div className="flex">
         <Sidebar />
-        <main className="">
+        <main className="bg-slate-50 flex-1 p-8">
           {/* children es donde react router en este caso inyectara la pagina actual*/}
           <Outlet />
         </main>
-
-        {/* <footer className="bg-slate-200 py-10 text-center text-slate-600 text-sm">
-          <div className="max-w-7xl mx-auto px-6">
-            <p>
-              &copy; {new Date().getFullYear()} Recipe Finder App — By{" "}
-              <span className="text-slate-800 font-medium">KevingaDev</span>
-            </p>
-          </div>
-        </footer> */}
       </div>
     );
 }
